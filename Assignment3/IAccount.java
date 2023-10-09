b91597640e5f36af7083b7ffada81664f2c38bb9
esnayem <esnayemmolla@gmail.com>
2023-10-09 20:35:16 +0600

    IAcccount Interface Created

---
+++ Assignment3/IAccount.java
@@ -0,0 +1,8 @@
+import java.math.BigDecimal;
+public interface IAccount {
+    public void deposit(BigDecimal amount);
+    public void withdraw(BigDecimal amount);
+    public void transfer(IAccount toAccount, BigDecimal amount);
+    public int getAccountNumber();
+    public BigDecimal showBalance();
+}
